715ea045ed6746890b9872c0c74f61b073c832c4
Роман Безуглий <romanbezuhlyi@MacBook-Pro-14.local>
2025-05-11 18:01:12 +0300

    gulp-webpack-starter version-1.0

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp')
+
+require('./gulp/dev.js')
+
+gulp.task(
+	'default',
+	gulp.series(
+		'clean:dev',
+		gulp.parallel(
+			'html:dev',
+			'sass:dev',
+			'images:dev',
+			'fonts:dev',
+			'files:dev',
+			'js:dev'
+		),
+		gulp.parallel('server:dev', 'watch:dev')
+	)
+)
+
+gulp.task(
+	'build',
+	gulp.series(
+		'clean:dev',
+		gulp.parallel(
+			'html:dev',
+			'sass:dev',
+			'images:dev',
+			'fonts:dev',
+			'files:dev',
+			'js:dev'
+		)
+	)
+)
